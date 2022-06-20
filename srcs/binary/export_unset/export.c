fa3067eff44683035d1e0a199205ccdf057a09b1
mtiesha <treceventashm@uandex.ru>
2022-06-20 14:13:44 +0300

    export fix

--- srcs/binary/export_unset/export.c
+++ srcs/binary/export_unset/export.c
@@ -6,30 +6,14 @@
 /*   By: mtiesha < mtiesha@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 11:24:24 by mtiesha           #+#    #+#             */
-/*   Updated: 2022/06/19 16:46:30 by mtiesha          ###   ########.fr       */
+/*   Updated: 2022/06/20 14:12:06 by mtiesha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-static int	ft_isinside(char **export, char *str)
+static void	ft_norm_export(t_src *s, char **ret, int i, int j)
 {
-	int	i;
-
-	i = 0;
-	while (export[i])
-	{
-		if (!ft_memcmp(export[i], str, ft_strlen(export[i])))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static void	ft_norm_export(t_src *s, char **ret)
-{
-	int	i;
-	int	j;
 	int	arg_count;
 
 	i = 0;
@@ -37,12 +21,15 @@ static void	ft_norm_export(t_src *s, char **ret)
 	arg_count = s->argc - 1;
 	while (s->export[i])
 	{
-		ret[i] = ft_strdup(s->export[i]);
+		if (ft_get_strnspl(s->argv, s->export[i], '='))
+			ret[i] = ft_strdup(ft_get_strnspl(s->argv, s->export[i], '='));
+		else
+			ret[i] = ft_strdup(s->export[i]);
 		i++;
 	}
 	while (arg_count)
 	{
-		if (!ft_isinside(s->export, s->argv[j]))
+		if (!ft_get_strnspl(s->export, s->argv[j], '='))
 		{
 			ret[i] = ft_strdup(s->argv[j]);
 			i++;
@@ -55,29 +42,27 @@ static void	ft_norm_export(t_src *s, char **ret)
 }
 
 int	ft_export_add(t_src *s)
-{// need add check if is include
+{
 	char	**ret;
 	int		i;
 	int		arg_count;
 
-	i = 1;// 0[export] 1[word1] 2[word2] 3[\0]
+	i = 1;
 	arg_count = (s->argc - 1);
 	while (arg_count)
 	{
 		if (!ft_isalpha(s->argv[i][0]))
 		{
-			ft_putstr_fd("bash: export: `", 1);
 			ft_putstr_fd(s->argv[i], 1);
-			ft_putstr_fd("': not a valid identifier\n", 1);
-			return (1);
+			return (1 && ft_putendl_fd(" :not a valid identifier [exp]", 1));
 		}
 		arg_count--;
 		i++;
 	}
 	arg_count = 0;
 	ft_count_include_av(s->export, s->argv, &arg_count);
-	i = (s->argc - 1 - arg_count) + ft_spllen(s->export);
+	i = (s->argc - 1 - arg_count) + ft_spllen(s->export) - arg_count;
 	ret = (char **)ft_calloc(sizeof(char *), i + 1);
-	ft_norm_export(s, &(*ret));
+	ft_norm_export(s, &(*ret), 0, 1);
 	return (0);
 }
